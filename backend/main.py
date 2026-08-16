3f38922706a65b2c544b0d917dd675c8cc5de788
KrilMax <kirill2446@gmail.com>
2026-08-16 20:57:16 +0200

    Save current backend state

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8080",
-        "http://127.0.0.1:8080",
+        "http://169.254.252.136:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
